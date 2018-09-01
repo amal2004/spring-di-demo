1ce147682772af0176ab0b434934dc323984028e
amal2004 <amal2004@gmail.com>
2018-09-01 15:36:17 -0400

    Addeed Configuration file

--- src/test/java/guru/springframework/DiDemoApplicationTests.java
+++ src/test/java/guru/springframework/DiDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring5.didemo;
+package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DiDemoApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
